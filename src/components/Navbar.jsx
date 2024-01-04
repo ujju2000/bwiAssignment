d596f612b08051125da27801f1d6c7c50b1f26f0
Ujjwal Tarika <ujjutarika.35@gmail.com>
2024-01-04 20:04:12 +0530

    changed routes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className = 'w-full bg-black p-4 flex items-center justify-around text-white cursor:pointer'>
-        <Link to = '/home'>Home</Link>
-        <Link  to  = '/login'>Login</Link>
+        <Link to = '/bwiAssignment/home'>Home</Link>
+        <Link  to  = '/bwiAssignment/login'>Login</Link>
     </div>
   )
 }
